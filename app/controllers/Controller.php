7a4b34c751ace81ef738f38d1ec430eee7b45b1f
matheuslesbao <matheuss.lesbao@gmail.com>
2024-02-01 16:32:59 -0300

    chore: modificação no controller

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -8,7 +8,7 @@ abstract class Controller
 {
     protected function view(string $view, array $data = [])
     {
-        $pathViews = dirname(__FILE__, 2) . DIRECTORY_SEPARATOR. 'views';
+        $pathViews = dirname(__FILE__, 2) . DIRECTORY_SEPARATOR. 'views' ;
         // Create new Plates instance
         $templates = new Engine($pathViews);
 
